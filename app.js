2b487fa4f2bf665c12bc37d6ab696675d48f40a1
Thiago Luz Alves de Matos <tmatos@uoldiveo.com>
2021-02-03 08:07:30 -0300

    Full Pipeline Test

--- app.js
+++ app.js
@@ -2,11 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-<<<<<<< HEAD
-app.get('/', (req, res) => res.send('Esse é meu primeiro projeto utilizando AWS'));
-=======
-app.get('/', (req, res) => res.send('Hello World'));
->>>>>>> eef45973d890ed060b4b49b1ae5ef3fe49e4006e
+app.get('/', (req, res) => res.send('Esse é meu primeiro projeto utilizando AWS usando pipelines'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
