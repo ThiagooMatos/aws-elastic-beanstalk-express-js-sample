3ff5b6fc467163f30301fc49b9e9aa686d14552e
ThiagooMatos <67804844+ThiagooMatos@users.noreply.github.com>
2021-02-03 07:41:25 -0300

    Update app.js

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ const port = 8080;
 
 <<<<<<< HEAD
 app.get('/', (req, res) => res.send('Esse é meu primeiro projeto utilizando AWS'));
-=======
-app.get('/', (req, res) => res.send('Hello World'));
->>>>>>> eef45973d890ed060b4b49b1ae5ef3fe49e4006e
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
